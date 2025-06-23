e0ef45eeb06e2c6484be98f70fee985d5989d37e
Ravim28 <ayushmalviya990@gmail.com>
2025-06-23 15:28:44 +0530

    server update

--- Backend/server.js
+++ Backend/server.js
@@ -39,8 +39,7 @@ cron.schedule('* * * * *', async () => {
 
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+
   })
   .then(() => {
     app.listen(port , () => console.log('🚀 Server running on port 5000'));
